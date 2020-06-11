7f15ba55596c27d7fc232f79396d1811de70b8bd
hnil <Halvor.M.Nilsen@sintef.no>
2020-06-11 09:26:25 +0200

    added SimulationType

--- opm/simulators/timestepping/AdaptiveTimeSteppingEbos.hpp
+++ opm/simulators/timestepping/AdaptiveTimeSteppingEbos.hpp
@@ -240,7 +240,7 @@ namespace Opm {
                 SimulatorReportSingle substepReport;
                 std::string causeOfFailure = "";
                 try {
-                    substepReport = solver.stepFull(substepTimer);
+                    substepReport = solver.step(substepTimer);
                     if (solverVerbose_) {
                         // report number of linear iterations
                         OpmLog::debug("Overall linear iterations used: " + std::to_string(substepReport.total_linear_iterations));
