025d6e98ad2ab72f169416e52ca622def8079e68
Kai Bao <kai.bao@sintef.no>
2020-08-13 00:36:19 +0200

    only writing extra_out for sequential running.

--- ebos/eclproblem.hh
+++ ebos/eclproblem.hh
@@ -1010,6 +1010,8 @@ public:
 
 
     void writeExtra(){
+        // Currently, the extra output is only for sequential simulation
+        if ( EWOMS_GET_PARAM(TypeTag, std::string, SimulationType) != "seq" ) return;
         std::string dir = this->simulator().problem().outputDir();
         if (dir == ".") {
             dir = "";
