4d3eda4df940197ad4c6af26cc435d09184d230d
hnil <Halvor.M.Nilsen@sintef.no>
2020-08-12 09:08:56 +0200

    go back to have state to be undersaturationed with rs=min(rsSat,rs) at border with derivative with respect to rs.
    - corrected update chopping sT
    - added asserts
    - output of iterarations to scrren
    - commented line on alternative inital guess for transport
    - skip well switching in transport

--- opm/simulators/flow/BlackoilModelEbos.hpp
+++ opm/simulators/flow/BlackoilModelEbos.hpp
@@ -895,6 +895,7 @@ namespace Opm {
                         msg += ") ";
                     }
                     OpmLog::debug(msg);
+                    OpmLog::info(msg);
                 }
                 std::ostringstream ss;
                 const std::streamsize oprec = ss.precision(3);
@@ -909,6 +910,7 @@ namespace Opm {
                 ss.precision(oprec);
                 ss.flags(oflags);
                 OpmLog::debug(ss.str());
+                OpmLog::info(ss.str());
             }
 
             return report;
